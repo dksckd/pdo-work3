94a3fbc6d4b4d1bd58b2dfcebd6e192327341902
dksckd <84056253+dksckd@users.noreply.github.com>
2021-12-14 14:27:16 +0900

    Update user-tbl.sql

--- user-tbl.sql
+++ user-tbl.sql
@@ -12,15 +12,17 @@ CREATE TABLE user (
     PRIMARY KEY(id)
 ) DEFAULT CHARSET=utf8mb4;
 /* テストデータ投入 */
-INSERT INTO user (email, name, gender) VALUE ('ichiro@xxx.com','山田一郎',1);
-INSERT INTO user (email, name, gender) VALUE ('jiro@xxx.com','山田次郎',1);
-INSERT INTO user (email, name, gender) VALUE ('ken@www.com','太田健一',1);
+INSERT INTO user (email, name, gender) VALUE ('ichiro@xxx.com','高橋一郎',1);
+INSERT INTO user (email, name, gender) VALUE ('jiro@xxx.com','尾木二郎',1);
+INSERT INTO user (email, name, gender) VALUE ('kenichi@www.com','小川健一',1);
 INSERT INTO user (email, name, gender) VALUE ('taro@bbb.jp','小川太郎',1);
-INSERT INTO user (email, name, gender) VALUE ('ohta@qqq.com','太田花子',2);
+INSERT INTO user (email, name, gender) VALUE ('thana@qqq.com','田中花子',2);
 INSERT INTO user (email, name, gender) VALUE ('hanako@sss.jp','小川花子',2);
 INSERT INTO user (email, name, gender) VALUE ('tanaka@xxx.com','田中一郎',1);
 INSERT INTO user (email, name, gender) VALUE ('chuo@xxx.com','中央次郎',1);
-INSERT INTO user (email, name, gender) VALUE ('ohji@www.com','太田次郎',1);
+INSERT INTO user (email, name, gender) VALUE ('sshiki@www.com','志木次郎',1);
 INSERT INTO user (email, name, gender) VALUE ('ken@bbb.jp','小川健',1);
-INSERT INTO user (email, name, gender) VALUE ('ohta@qqq.com','太田一郎',1);
-INSERT INTO user (email, name, gender) VALUE ('yuki@sss.jp','田中ゆき',2);
+INSERT INTO user (email, name, gender) VALUE ('asaka@qqq.com','朝霞一郎',1);
+INSERT INTO user (email, name, gender) VALUE ('miyuki@sss.jp','田中みゆき',2);
+INSERT INTO user (email, name, gender) VALUE ('tohoku@xxx.com','東北次郎',1);
+INSERT INTO user (email, name, gender) VALUE ('motoki@www.com','元木次郎',1);
