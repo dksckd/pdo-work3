cb9237c2920726aaca2841a0a2ad25aad5099f89
dksckd <84056253+dksckd@users.noreply.github.com>
2021-12-14 14:24:44 +0900

    Update list_u.php

--- list_u.php
+++ list_u.php
@@ -1,19 +1,18 @@
 <?php
-$limit = 5;
+session_start();
 require_once("functions.php");
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST["name"])){
-        if(!empty($_POST["name"])) {
-            $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+        if(! empty($_POST["name"])) {
+           $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+           $_SESSION["condition_name"] = $_POST["name"];
+        } else {
+           $_SESSION["condition_name"] = $_POST["name"];
         }
-        $page = 1;
-        $start = 0;
     }
-    
-}elseif($_SERVER['REQUEST_METHOD']==='GET'){
-        $page = isset($_GET['page']) ? $_GET['page'] : 1;
-        $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
-        $start = ($page - 1) * $limit;
+} else {
+    $name = $_SESSION["condition_name"];  //from edit.php
 }
 
 $dbh = db_conn();
@@ -56,16 +55,10 @@ try{
 <p>データ件数：<?php echo $count;?>件</p>
 
 <div class="container">
-<header>
-       <div>
-            <h1>ユーザー一覧</h1>
-       </div>
-    </header>
-</div>
-<hr>
-<p><?php echo $count;?>件見つかりました。</p>
+<form action=""edit.php method="POST">
+
 <table border=1>
-    <tr><th>id</th><th>名前</th><th>メールアドレス</th><th>性別</th><th>
+    <tr><th>id</th><th>名前</th><th>メールアドレス</th><th>性別</th><th>選択対象</th></tr>
     <?php foreach($data as $row): ?>
     <tr>
     <td><?php echo $row['id'];?></td>
@@ -82,53 +75,24 @@ try{
            }
         ?>
     </td>
+    <td>
+        <input type="radio" name="edit" value="<?php echo $row['id']; ?>">
+
+    </td>
     </tr>
     <?php endforeach; ?>
 </table>
 <p style="margin:8px;">
 
-<form action="" method="get">
-    <div>
-        <p>現在 <?php echo $page; ?> ページ目です。</p>
-        <?php 
+<p>編集するデータを選択してください</p>
 
-        $stmt = $dbh->prepare("SELECT COUNT(*) id FROM user WHERE name like :name"); 
-
-        $stmt->bindValue(':name', '%'.$name.'%', PDO::PARAM_STR); 
-        
-
-        $stmt->execute(); 
-
-        $page_num = $stmt->fetchColumn(); 
-
-        $pagination = ceil($page_num / $limit);
-        
-        ?>
-
-        <?php  
-
-            for ($x=1; $x <= $pagination ; $x++) { 
-
-                if ($x == $page){
-                    echo '<span>'.$x.'</span> | ';
-                }
-                else{
-                    echo ' '; 
-
-                    echo '<a href=?page='. $x. '&name='. $name.'>'. $x. '</a>'; 
-         
-                    echo ' '; 
-
-                }
-
-} ?> 
-</div>
-<div class="button-wrapper"> 
-
-<button type="button" onclick="history.back()">戻る</button> 
-
-</div> 
+        <div class="button-wrapper">
+            <button type="button" onclick="location.href='search_u.php'">戻る</button>
+	        <button type="submit" class="btn btn--naby btn--shadow">編集する</button>
+        </div>
 </form>
+</div>
+
 <hr>
 <div class="container">
     <footer>
